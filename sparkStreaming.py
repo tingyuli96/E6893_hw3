64b7d7a486049c59b6dc396ba71af3790ba85bca
colirain <904985646@qq.com>
2019-10-18 20:11:08 -0400

    hw3

--- sparkStreaming.py
+++ sparkStreaming.py
@@ -2,21 +2,23 @@
 # -*- coding: utf-8 -*-
 # Columbia EECS E6893 Big Data Analytics
 """
-This module is the spark streaming analysis process. 
+This module is the spark streaming analysis process.
 
 
 Usage:
     If used with dataproc:
         gcloud dataproc jobs submit pyspark --cluster <Cluster Name> twitterHTTPClient.py
-    
+
     Create a dataset in BigQurey first using
         bq mk bigdata_sparkStreaming
+
+    Remeber to replace the bucket with your own bucket name
 
 
 Todo:
     1. hashtagCount: calculate accumulated hashtags count
     2. wordCount: calculate word count every 10 seconds
-        the word you should track is listed below. 
+        the word you should track is listed below.
     3. save the result to google BigQuery
 
 """
@@ -36,18 +38,23 @@ bucket = ""    # TODO : replace with your own bucket name
 output_directory_hashtags = 'gs://{}/hadoop/tmp/bigquery/pyspark_output/hashtagsCount'.format(bucket)
 output_directory_wordcount = 'gs://{}/hadoop/tmp/bigquery/pyspark_output/wordcount'.format(bucket)
 
-# output table and columns name 
+# output table and columns name
 output_dataset = ''                     #the name of your dataset in BigQuery
 output_table_hashtags = 'hashtags'
 columns_name_hashtags = ['hashtags', 'count']
 output_table_wordcount = 'wordcount'
-columns_name_wordcount = ['word', 'count', 'timestamp']
+columns_name_wordcount = ['word', 'count', 'time']
+
+# parameter
+IP = 'localhost'    # ip port
+PORT = 9001       # port
+
+STREAMTIME = 600          # time that the streaming process runs
 
 WORD = ['data', 'spark', 'ai', 'movie', 'good']     #the words you should filter and do word count
 
 # Helper functions
 def saveToStorage(rdd, output_directory, columns_name, mode):
-
     """
     Save each RDD in this DStream to google storage
     """
@@ -77,12 +84,12 @@ def saveToBigQuery(sc, output_dataset, output_table, directory):
 def hashtagCount(words):
     """
     Calculate the accumulated hashtags count sum from the beginning of the stream
-    and sort it by descending order of the count. 
+    and sort it by descending order of the count.
     Ignore case sensitivity when counting the hashtags:
         "#Ab" and "#ab" is considered to be a same hashtag
     You have to:
-    1. Filter out the word that is hashtags. 
-       Hashtag usually start with "#" and followed by a serious of alphanumeric 
+    1. Filter out the word that is hashtags.
+       Hashtag usually start with "#" and followed by a serious of alphanumeric
     2. map (hashtag) to (hashtag, 1)
     3. sum the count of current DStream state and previous state
     4. transform unordered DStream to a ordered Dstream
@@ -94,26 +101,26 @@ def hashtagCount(words):
     Returns:
         DStream Object with inner structure (hashtag, count)
     """
-    
+
     # TODO: insert your code here
     pass
 
 def wordCount(words):
     """
-    Calculte the count of 5 sepcial words for every 20 seconds (window no overlap)
+    Calculte the count of 5 sepcial words for every 60 seconds (window no overlap)
     You can choose your own words.
     Your should:
     1. filter the words
     2. count the word during a special window size
-    3. add a time related mark to the output of each window, ex: a timestamp
+    3. add a time related mark to the output of each window, ex: a datetime type
     Hints:
         You can take a look at reduceByKeyAndWindow transformation
-        You may want to choose some common words to make sure it will appear 
-        during this time periods
+        Dstream is a serious of rdd, each RDD in a DStream contains data from a certain interval
+        You may want to take a look of transform transformation of DStream when trying to add a time
     Args:
         dstream(DStream): stream of real time tweets
     Returns:
-        DStream Object with inner structure (word, (count, timestamp))
+        DStream Object with inner structure (word, (count, time))
     """
 
     # TODO: insert your code here
@@ -139,7 +146,7 @@ if __name__ == '__main__':
     ssc.checkpoint("~/checkpoint_TwitterApp")
 
     # read data from port 9001
-    dataStream = ssc.socketTextStream("localhost",9001)
+    dataStream = ssc.socketTextStream(IP, PORT)
     dataStream.pprint()
 
     words = dataStream.flatMap(lambda line: line.split(" "))
@@ -162,13 +169,13 @@ if __name__ == '__main__':
     #   2. You may want to use helper function saveToStorage
     # TODO: insert your code here
 
-    # start streaming process, wait for 120s and then stop. 
+    # start streaming process, wait for 600s and then stop.
     ssc.start()
-    time.sleep(120)
+    time.sleep(STREAMTIME)
     ssc.stop(stopSparkContext=False, stopGraceFully=True)
 
     # put the temp result in google storage to google BigQuery
     saveToBigQuery(sc, output_dataset, output_table_hashtags, output_directory_hashtags)
     saveToBigQuery(sc, output_dataset, output_table_wordcount, output_directory_wordcount)
-    
+
 
